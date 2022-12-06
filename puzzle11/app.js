d2b0943e48be55eb6e468fb08cbf462fc5e7a368
Douglas Winter <d.winter@mapiq.com>
2022-12-06 22:18:05 +0100

    puzzles 11 and 12

---
+++ puzzle11/app.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+
+const readInput = () => {
+  try {
+    return fs.readFileSync("input.txt", "utf8");
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const toArray = (buffer) => buffer.split("");
+
+const isUnique = (packet) =>
+  packet.filter((value, index, array) => array.indexOf(value) === index)
+    .length === packet.length;
+
+const getPacket = (buffer, fromIndex, toIndex) => {
+  // Slice end is not inclusive
+  return toArray(buffer).slice(fromIndex, toIndex + 1);
+};
+
+const process = (buffer) => {
+  const packetSize = 4;
+
+  for (let packetIndex = 0; packetIndex < buffer.length; packetIndex++) {
+    const endOfPacket = packetIndex + packetSize;
+    const endOfPacketIndex = endOfPacket - 1;
+
+    if (endOfPacketIndex === buffer.length - 1) {
+      return "Start-of-packet marker not found";
+    }
+
+    const packet = getPacket(buffer, packetIndex, endOfPacketIndex);
+
+    if (isUnique(packet)) {
+      return endOfPacket;
+    }
+  }
+};
+
+const app = () => {
+  const buffer = readInput();
+  const result = process(buffer);
+
+  console.log(result);
+};
+
+app();
