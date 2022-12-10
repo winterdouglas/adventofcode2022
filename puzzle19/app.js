70f5061526d31ababdaf5e2563eacc45bd5db328
Douglas Winter <d.winter@mapiq.com>
2022-12-10 14:26:28 +0100

    puzzle 19 wip

---
+++ puzzle19/app.js
@@ -0,0 +1,131 @@
+const fs = require("fs");
+
+const readInput = () => {
+  try {
+    return fs.readFileSync("input.txt", "utf8");
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const toLines = (data) => data.split(/\r?\n/);
+
+const sum = (snapshot) => {
+  return Object.values(snapshot).reduce((acc, curr) => {
+    acc += curr;
+    return acc;
+  }, 0);
+};
+
+const Instruction = {
+  noop: "noop",
+  addx: "addx",
+};
+
+const toInstruction = (line) => {
+  const [i, v] = line.split(" ");
+  return i === Instruction.noop ? [i, 0] : [i, parseInt(v, 10)];
+};
+
+//const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
+const clock = async (onTick) => {
+  let cycle = 0;
+  let run = true;
+
+  const stop = () => {
+    run = false;
+  };
+
+  while (run) {
+    ++cycle;
+    if (onTick) onTick(cycle, stop);
+    //await sleep(50);
+  }
+};
+
+const process = (lines) => {
+  const instructions = lines.map(toInstruction);
+  let instructionIndex = 0;
+  let instructionCycles = 0;
+  let x = 1;
+  let snapshot = {
+    20: 0,
+    60: 0,
+    100: 0,
+    140: 0,
+    180: 0,
+    220: 0,
+  };
+
+  clock((cycle, stop) => {
+    if (snapshot[cycle] === 0) {
+      snapshot[cycle] = x * cycle;
+
+      console.log([cycle, x]);
+    }
+
+    if (instructionIndex === instructions.length - 1) {
+      stop();
+
+      //console.log(snapshot);
+      console.log(sum(snapshot));
+    }
+
+    const [ci, cv] = instructions[instructionIndex];
+    switch (ci) {
+      case Instruction.noop:
+        const noopCyles = 1;
+
+        if (instructionCycles < noopCyles) {
+          instructionCycles++;
+        }
+
+        if (instructionCycles === noopCyles) {
+          instructionIndex++;
+          instructionCycles = 0;
+        }
+
+        break;
+      case Instruction.addx:
+        const addxCycles = 2;
+
+        if (instructionCycles < addxCycles) {
+          instructionCycles++;
+        }
+
+        if (instructionCycles === addxCycles) {
+          x += cv;
+          instructionIndex++;
+          instructionCycles = 0;
+        }
+
+        break;
+      default:
+        throw Error("Instruction not recognized");
+    }
+
+    // if (snapshot[cycle] === 0) {
+    //   snapshot[cycle] = x * cycle;
+
+    //   console.log([cycle, x]);
+    // }
+
+    //console.log([cycle, x]);
+
+    // if (instructionIndex === instructions.length) {
+    //   stop();
+
+    //   //console.log(snapshot);
+    //   //console.log(sum(snapshot));
+    // }
+  });
+};
+
+const app = () => {
+  const data = readInput();
+  const lines = toLines(data);
+  const snapshot = process(lines);
+};
+
+app();
